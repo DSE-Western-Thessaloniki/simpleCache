67c0e8bdde870c9d2fa2b84cf71540e9db4f8889
Theofilos Intzoglou <int.teo@gmail.com>
2023-11-12 20:04:00 +0000

    Προσθήκη php docstring

--- src/Drivers/DriverInterface.php
+++ src/Drivers/DriverInterface.php
@@ -5,19 +5,41 @@ namespace Dsewth\SimpleCache\Drivers;
 use Psr\SimpleCache\CacheInterface;
 
 interface DriverInterface extends CacheInterface {
+	/**
+	 * Return the name of the driver.
+	 *  @return string
+	 */
 	public function getName(): string;
 
+	/**
+	 * Check whether the key is valid.
+	 * @param string $key 
+	 * @return bool 
+	 */
 	public function isValidKey(string $key): bool;
 
+	/**
+	 * Get the default TTL value 
+	 * @return int $ttl 
+	 */
 	public function getDefaultTtl(): int;
 
+	/**
+     * Set the default TTL value (in seconds)
+     * @param int $ttl 
+     */
 	public function setDefaultTtl(int $ttl): void;
 
 	/**
 	 * Return an array of all the keys stored in the cache.
-	 *  @return array  
+	 *  @return string[]  
 	 */
 	public function getKeys(): array;
 
+	/**
+     * Get the expiration timestamp for a key.
+     * @param string $key 
+     * @return int 
+     */
 	public function getExpirationTimestamp(string $key): int;
 }
